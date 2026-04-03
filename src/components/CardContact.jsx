2e53cd782d0d8df31b828fa6b08420daf21594e1
Marine-Briet <marinebriet@hotmail.com>
2026-04-03 20:13:51 +0200

    Structure and Content of the Home Page

---
+++ src/components/CardContact.jsx
@@ -0,0 +1,9 @@
+function CardContact() {
+    return (
+        
+    )
+}
+
+
+
+export default CardContact;
---
+++ src/components/HeroHome.jsx
@@ -0,0 +1,13 @@
+function HeroHome() {
+    return (
+        <div className="hero-home text-white">
+                <div className="hero-content text-center">
+                    <h1>Bonjour je suis John Doe</h1>
+                    <h2>Développeur web full stack</h2>
+                    <button type="button" className="btn btn-danger">En savoir plus</button>
+                </div>
+        </div>
+    )
+}
+
+export default HeroHome;
