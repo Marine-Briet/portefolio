2e1d900a0d534735ec9f3d2c3a24a1102b74d91d
Marine-Briet <marinebriet@hotmail.com>
2026-04-04 14:01:53 +0200

    Structure and Content of the Services Page

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+function Banner() {
+    return (
+        <div className="banner-generic">
+        </div>
+    )
+}
+
+export default Banner;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,13 @@
+import Banner from "../components/Banner";
+import PageIntro from "../components/PageIntro";
+
 function Portefolio() {
     return (
-        <div>
-            <h1>Portefolio</h1>
+        <div className="portfolio-page">
+            <Banner />
+            <div className="container">
+                <PageIntro title="Portfolio" content="Voici quelques unes de mes réalisations" />
+            </div>
         </div>
     );
 }
