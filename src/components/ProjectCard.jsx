d1401257a48b69690b711f6320787fa2b2162d18
Marine-Briet <marinebriet@hotmail.com>
2026-04-04 15:32:09 +0200

    Structure and Content of the Portfolio Page

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,15 @@
+function ProjectCard({title, content, titleClass, contentClass, skills, skillsClass, src, alt}) {
+    return (
+        <div className="border border-secondary-subtle rounded my-2 mx-1 pb-0 h-100 d-flex flex-column">
+            <img src={src} alt={alt} className="img-fluid w-100 rounded-top"/>
+            <h2 className={titleClass}>{title}</h2>
+            <p className={contentClass}>{content}</p>
+            <div>
+                <button className="btn btn-primary mb-3">Voir le site</button>
+            </div>
+            <p className={skillsClass}>{skills}</p>
+        </div>
+    )
+}
+
+export default ProjectCard;
